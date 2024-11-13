48996b3717b2bcc1432c7551be7689d6ae6124a1
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-11-13 22:10:54 +0330

    update todo action

--- src/server/actions/todo.action.ts
+++ src/server/actions/todo.action.ts
@@ -3,8 +3,10 @@ import { auth } from '../auth';
 import { prisma } from '../db';
 
 export async function getTodos(filter: string | null) {
+  const session = await auth();
   return await prisma.task.findMany({
     where: {
+      userId: session?.user.id,
       completed:
         filter === 'completed' ? true : filter === 'not' ? false : undefined, // If filter is "all" or null, it retrieves all tasks
     },
