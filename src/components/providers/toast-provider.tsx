f4b314e3af6b673c1aade9afddb04fdadec25fc0
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-11-14 12:15:04 +0330

    update toast style

--- src/components/providers/toast-provider.tsx
+++ src/components/providers/toast-provider.tsx
@@ -8,11 +8,12 @@ export const ToastProvider = () => {
       toastOptions={{
         style: {
           fontSize: 14,
-          fontWeight: 500,
+          fontWeight: 400,
           color: 'hsl(var(--foreground))',
           border: '1px solid hsl(var(--border))',
           backgroundColor: 'hsl(var(--card))',
           borderRadius: 'var(--radius)',
+          boxShadow: 'none',
         },
       }}
     />
